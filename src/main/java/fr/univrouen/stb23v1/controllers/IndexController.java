fc46c12e66d375e6c06d9d688d4f94a47114e105
Boufafa Lamis <il_boufafa@esi.dz>
2023-04-04 16:55:55 +0200

    fourt commit

--- src/main/java/fr/univrouen/stb23v1/controllers/IndexController.java
+++ src/main/java/fr/univrouen/stb23v1/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package fr.univrouen.stb23v1.controllers;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 
 public class IndexController {
